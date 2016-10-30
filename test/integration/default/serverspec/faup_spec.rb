10b023ad57fd0b787cdfbf061de2f91420b37e14
juju4 <juju4@users.noreply.github.com>
2016-10-30 16:16:43 -0400

    Initial commit on github

---
+++ test/integration/default/serverspec/faup_spec.rb
@@ -0,0 +1,26 @@
+require 'serverspec'
+
+# Required by serverspec
+set :backend, :exec
+
+## Use Junit formatter output, supported by jenkins
+#require 'yarjuf'
+#RSpec.configure do |c|
+#    c.formatter = 'JUnit'
+#end
+
+describe file('/usr/local/bin/faup') do
+  it { should be_executable }
+end
+
+describe command('/usr/local/bin/faup -v') do
+  its(:stdout) { should match /faup v/ }
+  its(:stdout) { should match /TLD file being used:/ }
+  its(:exit_status) { should eq 0 }
+end
+
+describe command('/usr/local/bin/faup -f tld slashdot.org') do
+  its(:stdout) { should match /org/ }
+  its(:exit_status) { should eq 0 }
+end
+
